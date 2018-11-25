801dc5d71767252083495a39fb733a31f864b564
Vitalii Cherkashyn <technik7job@gmail.com>
2018-11-25 08:36:02 +0100

    collaboration between bash and python

--- bash-to-python.sh
+++ bash-to-python.sh
@@ -1,2 +1,7 @@
 my_var="text from script"
+# send values as a part of script
 python -c "print('python processor:','$my_var')"
+
+# send values via pipe
+# one line execution
+cat bash-var-to-python.sh | python3 -c "exec('\nimport sys\nfor line in sys.stdin:\n  print(line)\n')"
