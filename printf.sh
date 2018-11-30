ed872d669c2e1fb6a5691232b5f01bab09cdf58f
technik <technik7job@gmail.com>
2018-11-30 05:53:01 +0100

    printf format for different types

--- printf.sh
+++ printf.sh
@@ -1,3 +1,6 @@
 var1=" >one< "
-var2=" >two< "
-printf "this is first var %s and second var %s" $var1 $var2
+var2=50
+var3=2.3337
+printf "this is first var %s and second var %d \n" $var1 $var2
+
+printf "%10s and second var %7d and third %10.2f \n" $var1 $var2 $var3
