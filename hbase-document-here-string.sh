7c24a86b7c287fb6c78faf2b94d4b04f7e2ed8ce
Vitalii Cherkashyn <technik7job@gmail.com>
2018-12-11 13:07:26 +0100

    substring, compare strings, split string to words

---
+++ hbase-document-here-string.sh
@@ -0,0 +1,22 @@
+# execute hbase with 'here document'
+result=`hbase shell << END_MARKER
+get "component-state","$1","meta:Ingest"
+exit
+END_MARKER`
+
+# read data from line of text
+echo $result | while read line;do
+    # split up line of text to words
+    for each_word in $line; do      
+        # substring of string, compare string
+        if [[ ${each_word:0:5} == value ]]; then
+            if [[ ${each_word:6} == Success ]]; then
+                echo "done"
+                exit(0)
+            else
+                echo "not done"
+                exit(1)
+            fi
+        fi
+    done
+done
