3ec3c551496fa3a890eb3baecd328a56b0970643
Vitalii Cherkashyn <technik7job@gmail.com>
2019-02-22 12:55:44 +0100

    folder reading

--- for.sh
+++ for.sh
@@ -2,3 +2,14 @@ END=5
 for ((i=1;i<=END;i++)); do
     echo $i
 done
+
+
+# example for loop over file list
+
+hdfs dfs -ls /some/hdfs/folder | awk '{print $8}' | while read each_folder
+do
+    if [[ $each_folder != "" ]]; then
+        echo $each_folder
+        hdfs dfs -ls $each_folder/*/*/*/*/*/* 
+    fi   
+done
