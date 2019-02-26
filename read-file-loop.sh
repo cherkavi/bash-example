223a2f5405e4a5394a7b73510033ee3f4492a78b
Vitalii Cherkashyn <technik7job@gmail.com>
2019-02-26 15:39:52 +0100

    loop over files from pipe

--- read-file-loop.sh
+++ read-file-loop.sh
@@ -1,4 +1,9 @@
+# print only files that ends with "completed" 
+# or start with: "completed*", but double quotas should not be used with comparation
+# print files by mask
 cat peptides.txt | while read line
 do
-   echo $line
+    if [[ $each_file == *completed ]]; then
+        echo $each_file
+    fi
 done
