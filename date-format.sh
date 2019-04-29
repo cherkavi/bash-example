38e7ea59685644d7f07255127a44a0bd31da17b8
Vitalii Cherkashyn <technik7job@gmail.com>
2019-04-29 15:25:42 +0200

    print current date and some previous

---
+++ date-format.sh
@@ -0,0 +1,5 @@
+date +'%d.%m.%Y %H:%M:%S'
+date +'%d.%m.%Y %H:%M:%S' --date="2 hour ago"
+date +'%d.%m.%Y' --date="2 day ago"
+date +'%d.%m.%Y' --date="2 month ago"
+date +'%d.%m.%Y' --date="2 year ago"
