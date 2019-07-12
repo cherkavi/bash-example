99483bac90228bb1ce2c82e240032d748006b358
Vitalii Cherkashyn <technik7job@gmail.com>
2019-07-12 11:33:25 +0200

    external varialbes

--- awk-FieldSeparator.sh
+++ awk-FieldSeparator.sh
@@ -1,1 +1,5 @@
 awk 'BEGIN{FS=","}{print $2}' column-data-comma.txt
+
+#                                print variable with number from external variable
+#     field separator from variable
+#awk -v FS=$main_file_delimiter '{print($'$main_file_field_number')}' | tr -d '\r' | tr -d '\a'`
