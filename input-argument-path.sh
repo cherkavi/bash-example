c37220200313e0f872164a485269a4c614ebf575
Vitalii Cherkashyn <technik7job@gmail.com>
2019-07-23 18:33:15 +0200

    filename without extension

--- input-argument-path.sh
+++ input-argument-path.sh
@@ -3,5 +3,11 @@ echo $full_path
 # echo ${full_path##*/}
 file_name="$(basename -- $full_path)"
 echo "$file_name"
+
+file_name_without_ext=${file_name%.*}
+echo "$file_name_without_ext"
+
 file_dir="$(dirname -- $full_path)"
 echo "$file_dir"
+
+
