0c3b34aa915ecaddbcc5e68bd165140eb6576698
Vitalii Cherkashyn <technik7job@gmail.com>
2019-08-30 16:45:22 +0200

    more that one delimiter, multi separator

---
+++ awk-multi-delimiter.sh
@@ -0,0 +1,5 @@
+ACTIVE_WINDOW_ID=`xdotool getactivewindow`
+ACTIVE_WINDOW_GEOMETRY=`xdotool getwindowgeometry $ACTIVE_WINDOW_ID`
+
+# using many delimiters, mutli-delimiter, multi separator with AWK
+echo $ACTIVE_WINDOW_GEOMETRY | awk -F "[ \,x]" '{print $4"  "$5}'
