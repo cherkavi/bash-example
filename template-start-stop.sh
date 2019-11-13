d460bdba679a1dfed2c4295d5b6dafab73552f1a
Vitalii Cherkashyn <technik7job@gmail.com>
2019-11-13 21:50:56 +0100

    start stop template

---
+++ template-start-stop.sh
@@ -0,0 +1,34 @@
+status() {
+    echo "==== Status"
+}
+
+start() {
+    echo "==== Start"
+}
+
+stop() {
+    echo "==== Stop"
+}
+
+
+case "$1" in
+    'start')
+            start
+            ;;
+    'stop')
+            stop
+            ;;
+    'restart')
+            stop ; echo "Sleeping..."; sleep 1 ;
+            start
+            ;;
+    'status')
+            status
+            ;;
+    *)
+            echo
+            echo "Usage: $0 { start | stop | restart | status }"
+            echo
+            exit 1
+            ;;
+esac
