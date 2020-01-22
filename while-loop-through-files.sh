f510c7dacf1e46fe3e5cc34a3e5b985046be322f
Vitalii Cherkashyn <technik7job@gmail.com>
2020-01-22 10:27:15 +0100

    list of sub-folders

--- while-loop-through-files.sh
+++ while-loop-through-files.sh
@@ -2,3 +2,9 @@ ls *.sh | while read each_file
 do
     ls -la $each_file | awk  '{print " "$5" - "$9}' >> file-sizes.txt
 done
+
+# list of sub-folders
+find /mapr/dp.prod.zurich/vantage/data/enriched/777111/2019 -maxdepth 3 -mindepth 3 | awk -F '/' '{print $14}' | while read each_session
+do 
+   echo $each_session
+done
