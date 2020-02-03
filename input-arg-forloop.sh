23b9fb30bf4ca9c68f207ece0c34c743f73c6d4e
Vitalii Cherkashyn <technik7job@gmail.com>
2020-02-03 05:46:58 +0100

    input arguments for loop

--- input-arg-forloop.sh
+++ input-arg-forloop.sh
@@ -1,7 +1,14 @@
-# summarize input arguments
+echo "# summarize input arguments"
 sum=0
 for each in "$@"
 do
    sum=$(( each + sum))
 done
 echo $sum
+
+args=("$@")
+echo "# print all arguments with positions"
+for (( index=0; index<$#; index++ ))
+do
+   echo $index"  "${args[@]:${index}:${index+1}}
+done
