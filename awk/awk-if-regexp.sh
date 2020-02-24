f9963d46e2d514772c9362232d704e645f268cf9
Vitalii Cherkashyn <technik7job@gmail.com>
2020-02-24 21:04:08 +0100

    multi condition

--- awk/awk-if-regexp.sh
+++ awk/awk-if-regexp.sh
@@ -1,2 +1,6 @@
 # find files with size more than Megabyte
 du -ah . | awk '{if($1 ~ /M/)print $1"  "$2}'
+
+
+# find all nodes for kubectl with specific status
+kubectl get pods --namespace training --output wide | awk '{if($7 ~ /gpu-1/ && $3 ~ /Running/)print $N}'
