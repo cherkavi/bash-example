8c0d9afbd4a7274d5567cd9de707b45338e85670
Vitalii Cherkashyn <technik7job@gmail.com>
2020-03-01 23:40:19 +0100

    ssh warning

--- while_loop_exec_for_each_line.sh
+++ while_loop_exec_for_each_line.sh
@@ -1,4 +1,6 @@
 head sessions-intel-empty.csv | awk -F ',' '{print $1}' | while read each_session
 do
 	echo $each_session" "`cat ../intel-filelist.txt | grep $each_session | wc -l`
+	# !!! warning for ssh !!!
+	# "ssh -n " instead of "ssh "
 done
