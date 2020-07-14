f162a6c91c0064db90838a74bd27039e865cb815
Vitalii Cherkashyn <technik7job@gmail.com>
2020-07-14 12:50:35 +0200

    execute script against all files in the list

--- for-loop-find.sh
+++ for-loop-find.sh
@@ -5,3 +5,10 @@ do
    printf "\n----------"$each_file"------------\n"
    cat $each_file
 done
+
+# execute script against file list
+cat list-of-files.txt | while read each_file
+do
+   result=`python2 check_files_for_osi.py $each_file`
+   echo $each_file"  "$result
+done
