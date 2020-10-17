f63596e431053f2fa42678b1831353ba266f2aae
Vitalii Cherkashyn <vitalii.cherkashyn@gmail.com>
2020-10-17 20:55:04 +0200

    for-loop simplification

--- for-loop-list.sh
+++ for-loop-list.sh
@@ -21,8 +21,9 @@ done
 # example of walking through list of environment variables, variable of variable, variable by name
 for each_val in "MYSQL_ROOT_PASSWORD" "MYSQL_USER" "MYSQL_PASSWORD"
 do
-    declare "current_variable=$each_val"
-    if [ -z ${!current_variable} ]; then
+    # declare "current_variable=$each_val"
+    # if [ -z ${!current_variable} ]; then
+    if [ -z $each_val ]
         echo "file .env should contains $each_val"
         exit 1
     fi
