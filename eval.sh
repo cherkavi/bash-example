27e0407e7786fcc54c4a3c4dfe44c614665c06fa
Vitalii Cherkashyn <technik7job@gmail.com>
2020-11-12 17:43:51 +0100

    eval

---
+++ eval.sh
@@ -0,0 +1,21 @@
+function evaluation() {
+        if [ $1 -ge 5 ]
+        then
+                eval $2
+                if [[ $? -eq 0 ]]
+                then
+                        echo "ok"
+                else
+                        echo "wrong command"
+                fi
+        else
+                echo "not"
+        fi
+}
+
+echo $command
+
+a=10
+# command='cat 1.txt'
+# not working: evaluation $a $command
+evaluation $a 'cat 1.txt' 
