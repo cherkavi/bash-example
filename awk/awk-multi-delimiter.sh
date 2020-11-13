b3946cf7422fa3f9308d27cd1f9bf3179cb48ee5
Vitalii Cherkashyn <technik7job@gmail.com>
2020-11-13 16:44:55 +0100

    between square brackets

--- awk/awk-multi-delimiter.sh
+++ awk/awk-multi-delimiter.sh
@@ -1,5 +1,5 @@
 ACTIVE_WINDOW_ID=`xdotool getactivewindow`
 ACTIVE_WINDOW_GEOMETRY=`xdotool getwindowgeometry $ACTIVE_WINDOW_ID`
 
-# using many delimiters, mutli-delimiter, multi separator with AWK
+# using many delimiters, mutli-delimiter, multi separator with AWK - between square brackets
 echo $ACTIVE_WINDOW_GEOMETRY | awk -F "[ \,x]" '{print $4"  "$5}'
