835afc04c07ceff895b72ffad4db1e51cbad1795
Vitalii Cherkashyn <vitalii.cherkashyn@gmail.com>
2020-11-15 13:32:10 +0100

    check if empty, test usage

--- if-argument.sh
+++ if-argument.sh
@@ -9,3 +9,21 @@ else
 fi
 # integer comparasion
 # https://www.tldp.org/LDP/abs/html/comparison-ops.html
+
+
+
+
+if [ -z "$1" ]
+then
+      echo "\$var is empty"
+else
+      echo "\$var is NOT empty"
+fi
+
+
+if test -z "$1" 
+then
+      echo "\$var is empty"
+else
+      echo "\$var is NOT empty"
+fi
