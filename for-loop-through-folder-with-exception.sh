e0c131d40dfd6a0c005a0dc60f9c6fe257750b06
Vitalii Cherkashyn <vitalii.cherkashyn@gmail.com>
2021-01-09 10:23:08 +0100

    for loop bash script

--- for-loop-through-folder-with-exception.sh
+++ for-loop-through-folder-with-exception.sh
@@ -1,3 +1,6 @@
+# for each_file in ls -v *.video.mp4
+# for each_file in $(ls -v *.video.mp4)
+
 for each_folder in  /some/folder/data_transfer/*/*/*/; do
    if [[ $each_folder == *data_transfer/VX7693/2018/10/11/ ]]; then
       continue
