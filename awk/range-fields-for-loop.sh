1b746a8d5654e17f40b0561819482abc4b6daf00
Vitalii Cherkashyn <vitalii.cherkashyn@gmail.com>
2021-01-14 23:51:31 +0100

    printf with for loop

--- awk/range-fields-for-loop.sh
+++ awk/range-fields-for-loop.sh
@@ -1,3 +1,5 @@
 # for loop
 # range of fields
 echo "/mnt/nfs/datasets/cityscapes_traffic_signs/training_cityscapes_tfs.h5" | awk -F "/" '{for(i=4; i<=(NF); i++){printf("/"$i)};printf("\n")}'
+
+# awk -F "/" '{for(i=4; i<=(NF); i++){printf("/%s", $i)};printf("\n")}'
