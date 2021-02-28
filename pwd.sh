864989b9e44c9f229b09f79c8edbb4c96a8d9e3c
Vitalii Cherkashyn <vitalii.cherkashyn@gmail.com>
2021-02-28 11:36:47 +0100

    detect current dir

--- pwd.sh
+++ pwd.sh
@@ -1,6 +1,15 @@
 # print current folder
 # working folder of execution
 
+# option 1
 current_directory=`pwd dirname $0`
 echo $current_directory
 
+# option 2
+full_path=`realpath $1`
+file_name="$(basename -- $full_path)"
+
+
+# real script example, current directory same directory
+current_directory=`dirname "$0"`
+python3 $current_directory/main.py $current_directory/email-sender.properties $*
