7f93e80a4f910d5ee1e919b1e60cc123d32954d5
Vitalii Cherkashyn <vitalii.cherkashyn@bmw.de>
2021-03-24 22:45:37 +0100

    while with external variable

--- while-break.sh
+++ while-break.sh
@@ -1,13 +1,23 @@
-ls | while read line
-do
-        if [ $line == 'drill' ]
-        then
-                echo "drill"
-                break
-        fi
-        echo $line
-done    
-
 # if you have loop with long running operation, than you can do next trick:
 # scp -r user@host.org/path/to/file || break
 # ctrl-c will lead to break the look
+echo "press control-c"
+ls | while read line
+do
+        echo "$line"
+        sleep 3 || break
+done    
+
+
+# change variable outside while
+last_line=""
+while read line
+do
+        if [ $line == 'function.sh' ]
+        then
+                echo "function.sh"
+                break
+        fi
+        last_line=$line
+done  < <( ls )
+echo "changed line >> "$last_line
