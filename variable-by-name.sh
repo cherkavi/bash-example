9b7ef7161796e73dba74484042d73ff0fc1505de
Vitalii Cherkashyn <vitalii.cherkashyn@bmw.de>
2021-04-22 17:56:29 +0200

    variable by name

---
+++ variable-by-name.sh
@@ -0,0 +1,30 @@
+## ------ simple variable -------
+# variable of variable
+var_name="my_variable"
+
+# my_variable=10
+eval "$var_name=10"
+
+# print it 
+echo "${!var_name}"
+eval "echo \$$var_name"
+
+
+
+## ------ array variable  -------
+var_name="my_array"
+eval "$var_name+=(1)"
+eval "$var_name+=(2)"
+eval "$var_name+=(3)"
+
+# print array
+eval "echo \${$var_name[@]}"
+
+# walk through array
+eval "output_array=(\${$var_name[@]})"
+echo "result length: ${#output_array[@]}"
+for each_value_in_array in "${output_array[@]}"; do    
+    echo $each_value_in_array
+    echo ">>>"
+done
+
