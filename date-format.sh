4968ce27ef697673dd96fd3864311f06be924078
Vitalii Cherkashyn <vitalii.cherkashyn@bmw.de>
2021-05-11 11:49:54 +0200

    parsing date

--- date-format.sh
+++ date-format.sh
@@ -4,3 +4,5 @@ date +'%d.%m.%Y %H:%M:%S' --date="2 hour ago"
 date +'%d.%m.%Y' --date="2 day ago"
 date +'%d.%m.%Y' --date="2 month ago"
 date +'%d.%m.%Y' --date="2 year ago"
+# parse date parse datetime
+date --date="15:21:51 04 Mar 2022" +"%Y.%m.%d %H:%M:%S"
