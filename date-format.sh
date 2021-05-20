ad8df5a6a580186e895d02c0b060a7a167024d59
Vitalii Cherkashyn <technik7job@gmail.com>
2021-05-20 16:33:06 +0200

    timestamp

--- date-format.sh
+++ date-format.sh
@@ -6,3 +6,7 @@ date +'%d.%m.%Y' --date="2 month ago"
 date +'%d.%m.%Y' --date="2 year ago"
 # parse date parse datetime
 date --date="15:21:51 04 Mar 2022" +"%Y.%m.%d %H:%M:%S"
+
+# datetime seconds linux time timestamp
+date -d @1621508129 '+%Y%m%d %H%M%S'
+TZ="America/Los_Angeles" date -d '2021-05-20 10:55:29' +%s
