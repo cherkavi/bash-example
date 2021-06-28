b50eceebd066213138d0e721a04e6a2244f2acaa
Vitalii Cherkashyn <vcherkashyn@wondersign.com>
2021-06-28 22:49:40 +0200

    left/inner join fix with test

--- join/inner-join.sh
+++ join/inner-join.sh
@@ -1,10 +1,11 @@
 # compare two files for equals strings
 # print equals lines
 # inner join for two files
+# for not exists take a look to: https://github.com/cherkavi/python-utilities/blob/master/console/unique-file-lines.py
 
 # one line bash approach: grep -F -f file1 file2
 file1=$1
 file2=$2
 cat $file2 | while read line; do
-    grep $line $file1 
+    grep "^${line}$" $file1
 done
