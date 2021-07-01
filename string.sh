4b0f06220fa9caba182407bf65c7766ea0d0ffc6
Vitalii Cherkashyn <vcherkashyn@wondersign.com>
2021-07-01 23:02:38 +0200

    additional tad

--- string.sh
+++ string.sh
@@ -3,4 +3,5 @@ string_value="just a brown fox fox and this fox is jumping over another fox"
 echo "${string_value//fox/dog}"
 
 # replace comma and double-quotas symbols
+# remove quotas
 echo "$a" | sed -r 's/[\"\,]+//g'
