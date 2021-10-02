91d09146d1f55bf17e24e693d239566782f5ca59
Vitalii Cherkashyn <vcherkashyn@wondersign.com>
2021-10-02 11:26:10 +0200

    color output to console

---
+++ color.sh
@@ -0,0 +1,8 @@
+# console color output color text color
+
+RED="\033[31m"
+CYAN="\033[36m"
+NO_COLOR="\033[m"
+
+
+printf "hello from ${RED} red color ${NO_COLOR} and ${CYAN} cyan color ${NO_COLOR}"
