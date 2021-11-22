dbbce0c6d65006f0c1212f2e77cd9ee1109cc480
Vitalii Cherkashyn <vitalii.cherkashyn@gmail.com>
2021-11-22 22:57:56 +0100

    press any key

--- read-char.sh
+++ read-char.sh
@@ -2,3 +2,7 @@
 # read one char from console
 read -s -p "Press any key to continue..." -n 1 input_char
 echo $input_char
+
+
+read -n 1 -s -r -p "Press any key to continue"
+read -p "Press enter to continue"
