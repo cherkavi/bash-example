568d86fb6c3a3aab27b8a8665486da2a0e7c3319
Vitalii Cherkashyn <vitalii.cherkashyn@gmail.com>
2021-11-30 12:25:48 +0100

    replace string

--- string.sh
+++ string.sh
@@ -12,3 +12,7 @@ export REPLACE_FOLDER='/mapr/prod/processed/'
 file_path='/home/projects/bmw/temp/mdf4-download/21401/2021/07/06/7b2a512c-1b98-43de/3100/ADCAM_MIPI/some_file.MF4'
 
 echo "${file_path/$LOCAL_MAP_FOLDER_PATH/$REPLACE_FOLDER}"
+
+# remove space in string
+a="a b c d"
+echo ${a// /}
