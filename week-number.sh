2c3edb4f7c7f6af785919788219435cd9b7bf34e
Vitalii Cherkashyn <vitalii.cherkashyn@gmail.com>
2022-01-26 10:11:12 +0100

    weeks in/before

--- week-number.sh
+++ week-number.sh
@@ -1,2 +1,9 @@
 # week number
 echo $(( ($(date +%-d)-1)/7+1))
+
+# two weeks ago
+echo $(( $(date +%V --date="2 week ago") ))
+
+# in two weeks
+echo $(( $(date +%V --date="2 week") ))
+echo $(( $(date +%V)+2 ))
