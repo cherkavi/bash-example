d381fbd805e54a269a58f44aaeb49a941a8f03da
Vitalii Cherkashyn <vitalii.cherkashyn@gmail.com>
2022-03-07 16:30:26 +0100

    gui elements menu

---
+++ gui-components-gui-menu.sh
@@ -0,0 +1,10 @@
+# gui elements for user input 
+
+# show file selection menu
+zenity --file-selection
+
+# show calendar menu
+zenity --calendar
+
+# show color selection menu
+zenity --color-selection
