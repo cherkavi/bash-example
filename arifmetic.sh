6908a3861b2c69dd051522e057dd1792c0a403a9
Vitalii Cherkashyn <vitalii.cherkashyn@gmail.com>
2022-08-06 08:19:36 +0200

    inline calculations

--- arifmetic.sh
+++ arifmetic.sh
@@ -3,6 +3,9 @@
 echo $(( 15 + 09 ))
 # bash: 15 + 09: value too great for base (error token is "09")
 
+# different base for digits 
 ❯ echo $(( 15 + 10#09 ))
 # 24
 
+# inline arifmetic operations 
+echo $( date +%G | cut -c3-4 )$( date +%V | xargs -I {} expr {} - 1 )
