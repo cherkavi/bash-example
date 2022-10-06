8ca12c0c131f316d7636936c5371edade731dfec
Vitalii Cherkashyn <vitalii.cherkashyn@gmail.com>
2022-10-06 14:45:47 +0200

    read multiple parameters inline

--- read.sh
+++ read.sh
@@ -1,3 +1,6 @@
+# read multiple parameters from 
+read VIN YEAR MONTH DAY  <<< `echo $MDF4_PATH | awk -F '/' '{print $9" "$10" "$11" "$12}'`
+
 # read parameters from string
 read -r col1 col2 col3 <<< "hello business world of IT"
 printf "col1: %s   col2: %s   col3 %s\n" "$col1" "$col2" "$col3"
