bbfb6cd0efedb2c7817bf24d86be4e66ee5f873f
Vitalii Cherkashyn <vitalii.cherkashyn@gmail.com>
2023-02-07 11:35:58 +0100

    jwt expiration time

--- jwt-decode.sh
+++ jwt-decode.sh
@@ -2,7 +2,11 @@
 function jwt_decode() {
     if [ -z $1 ]; then
         token=$KEYCLOAK_TOKEN
+    else 
+        token=$1
     fi
     my_array=($(echo $token | tr "." "\n"))
     echo ${my_array[1]} | base64 --decode | jq .
+
+    echo "expiration time: "`echo ${my_array[1]} | base64 --decode 2>/dev/null | jq . | grep exp | awk -F ':' '{print $2}' | tr ',' ' ' | xargs -I {} date -d @{} '+%Y-%m-%d %H:%M:%S'`
 }
