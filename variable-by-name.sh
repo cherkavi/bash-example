1fbbeff11f776a15705e452aebf96eb86d5cec49
Vitalii Ch <vitalii.cherkashyn@gmail.com>
2023-07-03 12:26:15 +0200

    password auto-complete

--- variable-by-name.sh
+++ variable-by-name.sh
@@ -29,3 +29,33 @@ for each_value_in_array in "${output_array[@]}"; do
     echo ">>>"
 done
 
+
+# -----------------
+# autocomplete example 
+# USER_LOAD2DESCR_PASSWORD
+# USER_DATA_CHECK_PASSWORD
+function dxc-users-password(){
+    PARAMETER=$1
+    if [ -z $PARAMETER ]; then
+        echo "first argument should be an unique part of the user name like 'data_check'"
+        return 1;
+    fi
+    amount_of_options=$(env | grep -e "_PASS=" -e "_PASSWORD=" | grep -i $PARAMETER | wc -l)
+    if [[ $amount_of_options > 1 ]]; then
+        echo "pls, specify name of the user more preciselly: $amount_of_options"
+        echo `env | grep "_PASS=" | grep $PARAMETER`
+        return 2
+    fi
+    env_variable=`env | grep -e "_PASS=" -e "_PASSWORD=" | grep -i $PARAMETER | awk -F '=' '{print $1}'`
+    env_value=`echo ${!env_variable}`
+    echo $env_value | clipboard
+    # echo $(eval "echo \$$env_variable")  | clipboard
+}
+
+function _dxc-users-password(){
+    if [[ ${COMP_CWORD} == 1 ]]; then            
+            COMPREPLY=`env | grep "USER_" | grep -e "_PASSWORD"| awk -F "=" '{print $1 }' `
+        return
+    fi
+}
+complete -F _dxc-users-password dxc-users-password
