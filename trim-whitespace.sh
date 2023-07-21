c3df204ab10667b4ec2349f03ef6beb45c9e975c
Vitalii Ch <vitalii.cherkashyn@gmail.com>
2023-07-21 17:57:31 +0200

    trim strip input value

--- trim-whitespace.sh
+++ trim-whitespace.sh
@@ -1,3 +1,6 @@
 # trim whitespace
 branch_name=`git status | head | grep 'On branch' | awk -F 'On branch' '{print $2}' | xargs`
 echo 'deploy branch: $branch_name'
+
+# trim strip remove white spaces
+trim_input_value=`echo $1 | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' `    
