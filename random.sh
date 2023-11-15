9bd310b386b0f158656e09d2f44754f11e1941c6
Vitalii Ch <vitalii.cherkashyn@gmail.com>
2023-11-15 18:02:05 +0100

    shuffle array

--- random.sh
+++ random.sh
@@ -1,3 +1,15 @@
 echo $RANDOM
 echo "-----------"
 echo $((1 + RANDOM % 10))
+
+
+echo "randomly change list, suffle list" 
+function team-random-list(){
+    original_array=( Mar Vit Mak Fra Dul And )
+    reshuffle_array=( $(shuf -e "${original_array[@]}") )
+    counter=0
+    for each_element in ${reshuffle_array[@]}; do
+        ((counter++))
+        echo "$counter. $each_element"
+    done 
+}
