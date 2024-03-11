c307360565de3f181de75c0b688154a05c8bbadc
Vitalii Ch <vitalii.cherkashyn@gmail.com>
2024-03-11 06:22:31 +0100

    trim single quotas

--- trim-whitespace.sh
+++ trim-whitespace.sh
@@ -4,3 +4,6 @@ echo 'deploy branch: $branch_name'
 
 # trim strip remove white spaces
 trim_input_value=`echo $1 | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' `    
+
+# trim strip single quotas 
+echo 'hello' | jq . 2>/dev/null
