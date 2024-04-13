cbb3b0db1fbab75dc9b646046d8c1ca60456033a
Vitalii Cherkashyn <vitalii.cherkashyn@gmail.com>
2024-04-13 11:35:11 +0200

    bash post processing

---
+++ bash-post-processing.sh
@@ -0,0 +1,20 @@
+## bash command aspect, execute after each bash command, bash command post processing
+
+## analyse last executed command for having anchors at the beginning and if yes - re-execute without anchors
+## post executor
+# export PROMPT_COMMAND='starship_precmd'
+function last_command_check_anchor(){
+    ### jira 157558  # epic: jira 91962
+    last_command=`history | tail -n 1 | head -n 1 | awk '{$1=""; print $0}'`        
+    if echo $last_command | grep -q "^##";  then
+        echo "last command is possible anchor: $last_command"
+        # echo "$last_command" | awk '{$1=""; print $0}'
+        last_command=`echo "$last_command" | awk '{$1=""; print $0}'`
+        $last_command
+    fi
+}
+function activate-last-command-analyzer(){
+    export PROMPT_COMMAND='last_command_check_anchor'
+}
+
+
